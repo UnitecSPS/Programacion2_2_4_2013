628e0b1c3ca6ae181b69ea04d1f2569b86721272
Carlos Gochez <carlos.gochez@unitec.edu>
2013-07-18 17:26:49 -0600

    Agregar clase test

---
+++ src/TestGit.java
@@ -0,0 +1,18 @@
+
+import java.util.Calendar;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gotcha
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        Calendar c;
+        System.out.println("HOLA");
+    }
+}

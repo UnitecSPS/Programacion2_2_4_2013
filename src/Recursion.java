89b1e73e55407208a74993061b7c492513f76ea8
Carlos Gochez <carlos.gochez@unitec.edu>
2013-07-18 17:49:34 -0600

    Agregue clase de recursion

---
+++ src/Recursion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gotcha
+ */
+public class Recursion {
+    public void imp(int x){
+        
+    }
+}

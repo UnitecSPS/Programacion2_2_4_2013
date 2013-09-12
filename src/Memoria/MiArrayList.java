9cb648a863e56d79cdee4c5e93c5e1464611ebf0
Carlos Gochez <carlos.gochez@unitec.edu>
2013-09-12 09:41:54 -0600

    Mi Arraylist

--- src/Memoria/MiArrayList.java
+++ src/Memoria/MiArrayList.java
@@ -9,5 +9,19 @@ package Memoria;
  * @author Gotcha
  */
 public class MiArrayList {
+    Nodo inicio = null;
     
+    public void add(Nodo obj){
+        if( inicio == null ){
+            inicio = obj;
+        }
+        else{
+            Nodo ultimo = inicio;
+            
+            while(ultimo.siguiente != null ){
+                ultimo = ultimo.siguiente;
+            }
+            ultimo.siguiente = obj;
+        }
+    }
 }
